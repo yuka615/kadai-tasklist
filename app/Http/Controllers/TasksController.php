19ca8e7657720eccac11b4ddcc14c962f7b20bc1
yuka.yanagawa <yuka.yanagawa@rakuten.com>
2018-05-31 05:29:09 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -46,7 +46,12 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
-         $task = new Task;
+        
+        $this->validate($request, [
+            'content' => 'required|max:10',
+        ]);
+        
+        $task = new Task;
         $task->content = $request->content;
         $task->save();
 
@@ -92,6 +97,11 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        
+        $this->validate($request, [
+            'content' => 'required|max:191',
+        ]);
+        
         $task = Task::find($id);
         $task->content = $request->content;
         $task->save();
