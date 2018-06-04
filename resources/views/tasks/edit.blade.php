d7ba94f2c6b1dc3832a18e1c6e46d635a38c118d
yuka.yanagawa <yuka.yanagawa@rakuten.com>
2018-06-04 05:09:20 +0000

    Procfile

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -17,7 +17,7 @@
         {!! Form::text('content', null, ['class' => 'form-control']) !!}
     </div> 
 
-        {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+        {!! Form::submit('Update', ['class' => 'btn btn-info']) !!}
 
     {!! Form::close() !!}
 </div>
