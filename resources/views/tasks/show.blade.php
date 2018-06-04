fdc3a826a382be38e763b8990dd93e2f2ac54e40
yuka.yanagawa <yuka.yanagawa@rakuten.com>
2018-06-04 05:55:50 +0000

    modified show

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -6,14 +6,19 @@
     <table class="table table-striped">
         <div class="row">
         <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-md-6">
-    <thead>
                 <tr>
-                    <th>id</th>
-                    <th>進捗</th>
-                    <th>タスク</th>
-                </tr>
-            </thead>
-            </table>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>進捗</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
     
     {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
 
