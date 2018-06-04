f81dd383aa6c8519cd8468a4416e565bafd69996
yuka.yanagawa <yuka.yanagawa@rakuten.com>
2018-06-04 05:44:46 +0000

    modified index

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -19,7 +19,7 @@
         <tbody>
             @foreach ($tasks as $task)
             <tr>
-                <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->status }} > {{ $task->content }}</td>
+                <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
                 <td>{{ $task->status }}</td>
                         <td>{{ $task->content }}</td>
             @endforeach
