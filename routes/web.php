839e71cd92f8814d3c0c4e0986019beb7dcbaf85
yuka.yanagawa <yuka.yanagawa@rakuten.com>
2018-06-13 06:56:28 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,17 @@
 |
 */
 
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
 
 Route::get('/', function () {
     return view('welcome');
+    
 });
+    
+
+
