6e0f61776b13de3dd2cc0d8d8150432c6af25240
yuka.yanagawa <yuka.yanagawa@rakuten.com>
2018-06-14 01:44:10 +0000

    modified web.php

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => 'auth'], function () {
     // Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
-    Route::resource('tasks', 'TasksController', ['only' => ['store', 'destroy']]);
+    Route::resource('tasks', 'TasksController');
 });
     
 
